97062c6249f30c5b0289e8c7d60b7e602a3d743c
vovaushenko <vovaushenko1989@gmail.com>
2020-10-22 21:59:48 +0300

    6 kyu String array duplicates

---
+++ 6kyu_String array duplicates.js
@@ -0,0 +1,27 @@
+//https://www.codewars.com/kata/59f08f89a5e129c543000069
+
+function dup(s) {
+  let bigStr = s.join(' ');
+
+  let p1 = 0;
+  let p2 = 1;
+  let input = bigStr + '1';
+
+  let antiDup = '';
+  while (p2 < input.length) {
+    if (input[p1] === input[p2]) {
+      p1++;
+    }
+    if (input[p1] !== input[p2]) {
+      antiDup += input[p1];
+      p1++;
+    }
+
+    p2++;
+  }
+  let solution = antiDup.split(' ');
+  return solution;
+}
+console.log(
+  dup(['ccooddddddewwwaaaaarrrrsssss', 'piccaninny', 'hubbubbubboo'])
+);
