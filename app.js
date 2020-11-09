c399310cc148ca71362cc29ef5ee2a9148b78a76
vovaushenko <vovaushenko1989@gmail.com>
2020-11-09 22:01:10 +0200

    123

--- app.js
+++ app.js
@@ -1,26 +1,18 @@
-// https://www.codewars.com/kata/57b6f5aadb5b3d0ae3000611
+// https://www.codewars.com/kata/5898b4b71d298e51b600014b
 
-const getLengthOfMissingArray = (arrOfArr) => {
-  if (
-    arrOfArr === null ||
-    arrOfArr.length === 0 ||
-    arrOfArr.includes(null) === true
-  ) {
-    return 0;
-  }
-  let arrOfLengths = [];
-  for (let arr of arrOfArr) {
-    arrOfLengths.push(arr.length);
-  }
-  arrOfLengths.sort((a, b) => a - b);
-  if (arrOfLengths[0] === 0) {
-    return 0;
-  }
+// const sortTheInnerContent = (str) => {
+//   let words = str.split(' ');
+//   console.log(words);
+//   let solution = [];
+//   for (let word of words) {
+//     console.log(word);
+//     let sort = [...word].sort((a,b)=>b-a)
+//   }
+// };
 
-  for (let i = 0; i < arrOfLengths.length; i++) {
-    if (arrOfLengths[i + 1] - arrOfLengths[i] === 2) {
-      return arrOfLengths[i] + 1;
-    }
-  }
-};
-console.log(getLengthOfMissingArray(null));
+// sortTheInnerContent('sort the inner content in descending order');
+
+const word = 'sort';
+
+let sort = [...word].sort((a, b) => a - b);
+console.log(sort);
