9c504ebf2821df4b62bd119bc4002860a01ecee3
vovaushenko <vovaushenko1989@gmail.com>
2020-11-21 14:30:45 +0200

    123

--- app.js
+++ app.js
@@ -1,6 +1,1 @@
-var removeDuplicates = function(nums) {
-  return [... new Set(nums)]
-};
-
-
-console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
+// https://www.codewars.com/kata/5a523566b3bfa84c2e00010b
