18e8f303a5613215edf0c32cc79e5e70a7d7aec4
vovaushenko <vovaushenko1989@gmail.com>
2020-11-24 15:50:54 +0200

    6 kyu Let's Recycle!

--- 6 kyu Let's Recycle.js
+++ 6 kyu Let's Recycle.js
@@ -1,23 +1,17 @@
 // https://www.codewars.com/kata/5b6db1acb118141f6b000060
 
 const recycle = (input) => {
-  let paper = input
-    .filter((a) => a.material === "paper" || a.secondMaterial === "paper")
-    .map((obj) => obj.type);
+  const recycling = (material) =>
+    input
+      .filter((a) => a.material === material || a.secondMaterial === material)
+      .map((obj) => obj.type);
 
-  let glass = input
-    .filter((a) => a.material === "glass" || a.secondMaterial === "glass")
-    .map((obj) => obj.type);
-
-  let organic = input
-    .filter((a) => a.material === "organic" || a.secondMaterial === "organic")
-    .map((obj) => obj.type);
-
-  let plastic = input
-    .filter((a) => a.material === "plastic" || a.secondMaterial === "plastic")
-    .map((obj) => obj.type);
-
-  let recycled = [paper, glass, organic, plastic];
+  let recycled = [
+    recycling("paper"),
+    recycling("glass"),
+    recycling("organic"),
+    recycling("plastic"),
+  ];
 
   return recycled;
 };
