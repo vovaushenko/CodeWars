3e1d06f6bacd33bbd76c0564d26f953e75aff6c8
vovaushenko <vovaushenko1989@gmail.com>
2020-11-25 19:42:47 +0200

    7 kyu STRONGN Strong Number (Special Numbers Series #2)

--- 7 kyu STRONGN Strong Number (Special Numbers Series #2).js
+++ 7 kyu STRONGN Strong Number (Special Numbers Series #2).js
@@ -1,14 +1,14 @@
 // https://www.codewars.com/kata/5a4d303f880385399b000001
 
+
+
 const strong = (num) => {
   let sumOfFactorials = Array.from(String(num), Number)
     .map((num) =>
-      Array.from({ length: num }, () => num--).reduce(
-        (product, num) => product * num,
-        1
-      )
-    )
+      Array.from({ length: num }, () => num--)
+      .reduce((product, num) => product * num, 1))
     .reduce((total, num) => total + num);
+  
 
   return sumOfFactorials == num ? 'STRONG!!!!' : 'Not Strong !!';
 };
