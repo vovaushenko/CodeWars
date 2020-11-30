d0ee729326e40c46cd6bf801455a1ad4ca6a1f38
vovaushenko <vovaushenko1989@gmail.com>
2020-11-30 15:13:06 +0200

    6 kyu Transform To Prime

--- 6 kyu Transform To Prime.js
+++ 6 kyu Transform To Prime.js
@@ -1,7 +1,5 @@
 // https://www.codewars.com/kata/5a946d9fba1bb5135100007c
 
-// firstly we will write isPrime function
-
 const isPrime = (num) => {
   for (i = 2; i < num; i++) {
     if (num % i === 0) return false;
@@ -10,7 +8,7 @@ const isPrime = (num) => {
 };
 
 const minimumNumber = (arr) => {
-  let originalSum = arr.reduce((a, b) => a + b);
+  let originalSum = arr.reduce((a, b) => a + b, 0);
 
   let sum = originalSum;
   while (!isPrime(sum)) {
@@ -18,5 +16,3 @@ const minimumNumber = (arr) => {
   }
   return sum - originalSum;
 };
-
-console.log(minimumNumber([]));
