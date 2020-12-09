88194bc805461c851e900473eb0a0e5200951a82
vovaushenko <vovaushenko1989@gmail.com>
2020-12-09 14:42:27 +0200

    7 kyu Responsible Drinking

--- 7 kyu Responsible Drinking.js
+++ 7 kyu Responsible Drinking.js
@@ -7,5 +7,3 @@ const hydrate = (s) => {
     .reduce((a, b) => a + parseInt(b), 0);
   return total !== 1 ? `${total} glasses of water` : '1 glass of water';
 };
-
-console.log(hydrate('1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer'));
