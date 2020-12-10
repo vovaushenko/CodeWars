c147fea8760d6f30468dc1626a8020ea21492a84
vovaushenko <vovaushenko1989@gmail.com>
2020-12-10 19:21:55 +0200

    7 kyu Split In Parts

--- 7 kyu Split In Parts.js
+++ 7 kyu Split In Parts.js
@@ -1,7 +1,5 @@
 // https://www.codewars.com/kata/5650ab06d11d675371000003
 
-// lets practice recursion
-
 const splitInParts = (s, partLength) => {
   let result = '';
 
@@ -14,5 +12,3 @@ const splitInParts = (s, partLength) => {
   splitter(s);
   return result.trim();
 };
-
-splitInParts('HelloKata', 2);
