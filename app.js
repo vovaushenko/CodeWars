9b930088fe47035b57df4d00c9be7a0d63624179
vovaushenko <vovaushenko1989@gmail.com>
2020-12-12 21:16:24 +0200

    7 kyu Return String of First Characters

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-// https://www.codewars.com/kata/5834315e06f227a6ac000099
+// https://www.codewars.com/kata/5639bdcef2f9b06ce800005b
 
-const elimination = (arr) => {
-  const map = {};
-  for (let num of arr) {
-    map[num] ? map[num]++ : (map[num] = 1);
-  }
-
-  for (let key in map) {
-    if (map[key] === 2) return Number(key);
-  }
-
-  return null;
-};
+const makeString = s => s.split(' ').map(a => a[0]).join('')
