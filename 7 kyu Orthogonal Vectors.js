61aed12ab1f0d76132e47f6a5ffcad72ac8dcc82
vovaushenko <vovaushenko1989@gmail.com>
2020-12-13 15:14:11 +0200

    7 kyu Orthogonal Vectors

---
+++ 7 kyu Orthogonal Vectors.js
@@ -0,0 +1,4 @@
+// https://www.codewars.com/kata/53670c5867e9f2222f000225
+
+const isOrthogonal = (u, v) =>
+  u.map((a, id) => a * v[id]).reduce((a, b) => a + b, 0) === 0 ? true : false;
--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-String.prototype.repeat = function (count) {
-  return Array.from({ length: count }, () => this).join('');
-};
+// https://www.codewars.com/kata/53670c5867e9f2222f000225
+
+const isOrthogonal = (u, v) =>
+  u.map((a, id) => a * v[id]).reduce((a, b) => a + b, 0) === 0 ? true : false;
+
+console.log(isOrthogonal([1, -2], [2, 1]));
