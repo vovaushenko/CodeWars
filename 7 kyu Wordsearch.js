5e9e2841c2d01dff5e2d0a6beca667c0bf9a5db5
vovaushenko <vovaushenko1989@gmail.com>
2020-12-13 15:44:21 +0200

    7 kyu Wordsearch

---
+++ 7 kyu Wordsearch.js
@@ -0,0 +1,3 @@
+// https://www.codewars.com/kata/5545f847bd94d050be00011d
+
+const wordSearch = (word, text) =>  text.split(/([_\W])/).includes(word)
--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-// https://www.codewars.com/kata/53670c5867e9f2222f000225
+const hammingWeight = (num) => [...(num.toString(2) + '')].filter((num) => Number(num)).length;
 
-const isOrthogonal = (u, v) =>
-  u.map((a, id) => a * v[id]).reduce((a, b) => a + b, 0) === 0 ? true : false;
-
-console.log(isOrthogonal([1, -2], [2, 1]));
+console.log(hammingWeight(1867824 ));
