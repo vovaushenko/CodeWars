2a3595f8228da81fe2ad1cd808697ef79cef3660
vovaushenko <vovaushenko1989@gmail.com>
2020-12-21 19:36:53 +0200

    7 kyu Numbers to Letters

--- 7 kyu Numbers to Letters.js
+++ 7 kyu Numbers to Letters.js
@@ -1,10 +1,7 @@
 // https://www.codewars.com/kata/57ebaa8f7b45ef590c00000c
 
 const switcher = (numbers) => {
-  // const ALPHABET = ' abcdefghijklmnopqrstuvwxyz!? ';
   const ALPHABET = 'zyxwvutsrqponmlkjihgfedcba!? ';
 
   return numbers.map((num) => ALPHABET[num - 1]).join('');
 };
-
-console.log(switcher(['25', '7', '8', '4', '14', '23', '8', '25', '23', '29', '16', '16', '4']));
