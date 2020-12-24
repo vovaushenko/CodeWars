f25540d7b3a7bad650bcfcfdd4b07153e2c3c4fc
vovaushenko <vovaushenko1989@gmail.com>
2020-12-24 18:19:42 +0200

    5 kyu Pete, the baker

--- 5 kyu Pete, the baker.js
+++ 5 kyu Pete, the baker.js
@@ -10,4 +10,3 @@ const cakes = (recipe, availableIngredients) => {
 
   return Math.min(...numberOfCakes);
 };
-cakes({ flour: 500, sugar: 200, eggs: 1 }, { flour: 1200, sugar: 1200, eggs: 5, milk: 200 });
