899b888ee6144e1a82e47d9cb5a3e11f998c407c
vovaushenko <vovaushenko1989@gmail.com>
2020-12-26 14:56:14 +0200

    5 kyu Not very secure

---
+++ 5 kyu Not very secure.js
@@ -0,0 +1,3 @@
+// https://www.codewars.com/kata/526dbd6c8c0eb53254000110
+
+let alphanumeric = (s) => /^[a-z\d]+$/i.test(s);
--- app.js
+++ app.js
@@ -1,23 +1,10 @@
-const drawACross = (n) => {
-  if (n < 3) return 'Not possible to draw cross for grids less than 3x3!';
-  if (n % 2 == 0) return 'Centered cross not possible!';
-
-  let cross = '';
-  for (let i = 0; i < n; i++) {
-    cross += drawLine(n, i) + '\n';
+const alphanumeric = (str) => {
+  if (!str.length) return false;
+  const ALLOWED_CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  for (let char of str) {
+    if (!ALLOWED_CHARS.includes(char)) return false;
   }
-  return cross.trim();
+  return true;
 };
 
-function drawLine(len, position) {
-  let line = Array.from({ length: len }, (_, id) => {
-    if (id === position) {
-      return 'x';
-    } else if (id === len - position - 1) {
-      return 'x';
-    } else {
-      return ' ';
-    }
-  });
-  return line.join('');
-}
+console.log(alphanumeric('dasda sa'));
