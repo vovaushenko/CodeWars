227b01801ee018e6522a7b20edfa5444de9b36af
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-01-07 20:35:34 +0200

    6 kyu Simple frequency sort

--- 6 kyu Simple frequency sort.js
+++ 6 kyu Simple frequency sort.js
@@ -1,12 +1,6 @@
 // https://www.codewars.com/kata/5a8d2bf60025e9163c0000bc
 
-// firstly we will create hash-map of the array
-// then we will do some basic sort based on frequency
-
 const solve = (arr) => {
     const hash = arr.reduce((h, c) => ((h[c] = h[c] + 1 || 1), h), {});
     return arr.sort((a, b) => (hash[b] !== hash[a + ''] ? hash[b + ''] - hash[a + ''] : a - b));
 };
-
-console.log(solve([2, 3, 5, 3, 7, 9, 5, 3, 7]));
-// [3,3,3,5,5,7,7,2,9]
