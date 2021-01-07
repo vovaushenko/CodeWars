05a793cd6de2338d23d896bf2b13d17dab230ad8
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-01-07 19:52:14 +0200

    6 kyu Don't rely on luck.

---
+++ 6 kyu Don't rely on luck.js
@@ -0,0 +1,4 @@
+// https://www.codewars.com/kata/5268af3872b786f006000228
+
+var guess = 77701;
+Math.random = () => 777;
--- app.js
+++ app.js
@@ -1,32 +1,10 @@
-// https://www.codewars.com/kata/52755006cc238fcae70000ed
+// https://www.codewars.com/kata/54b80308488cb6cd31000161
 
-const generateRow = (h, index) => {
-    const row = Array.from({ length: h * 2 - 1 }, () => ' ');
-    const mid = Math.floor(row.length / 2);
-    row[mid] = '*';
-    let i = mid,
-        j = mid;
-    let counter = 0;
-    while (counter != index + 1) {
-        row[i] = '*';
-        row[j] = '*';
-        i++;
-        j--;
-        counter++;
-    }
+const groupCheck = (str) => {
+    const map = {};
+    for (let char of str) map[char] ? map[char]++ : (map[char] = 1);
 
-    return row.join('');
+    
 };
 
-const christmasTree = (n) => {
-    let tree = '';
-    for (let i = 0; i < n; i++)
-        i !== n - 1 ? (tree += generateRow(n, i) + '\n') : (tree += generateRow(n, i));
-
-    return tree;
-};
-// An = a1 + (n-1)*d
-
-//   A3 = 1 + 2*2
-
-console.log(christmasTree(5));
+groupCheck('{(})');
