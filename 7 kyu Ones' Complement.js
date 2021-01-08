8683af6979784b7944deabdc3020a540e7a0b571
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-01-08 17:15:54 +0200

    7 kyu Ones' Complement

--- 7 kyu Ones' Complement.js
+++ 7 kyu Ones' Complement.js
@@ -1,5 +1,3 @@
 // https://www.codewars.com/kata/59b11f57f322e5da45000254
 
 const onesComplement = (bin) =>[...(bin + '')].map((bit) => (bit === '0' ? (bit = '1') : (bit = '0'))).join('');
-
-console.log(onesComplement(1001));
