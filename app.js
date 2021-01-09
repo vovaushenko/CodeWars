4cd6857a67b16c1d3e29ee31be9d631641d4c02a
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-01-09 16:08:56 +0200

    7 kyu Breaking chocolate problem

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-// https://www.codewars.com/kata/54ca3e777120b56cb6000710
+// https://www.codewars.com/kata/534ea96ebb17181947000ada
 
-const chained = (functions) => {
-    return (input) => {
-        let res = functions[0](input);
-        for (let i = 1; i < functions.length; i++) {
-            res = functions[i](res);
-        }
-
-        return res;
-    };
-};
+const breakChocolate = (n, m) => n && m ? n * m - 1 : 0;
