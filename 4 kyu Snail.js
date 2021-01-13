3a1df804edd92048f9c00e29c79b4b5c5e6a1244
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-01-13 21:49:12 +0200

    4 kyu Snail

--- 4 kyu Snail.js
+++ 4 kyu Snail.js
@@ -2,6 +2,7 @@
 
 const chenckLength = (arr) => arr.reduce((total, a) => total + a.length, 0);
 
+// will later refactor this mess 🤣🤪
 const snail = (arr) => {
     let res = [];
     let count = 0;
@@ -43,13 +44,3 @@ const snail = (arr) => {
     helper(arr);
     return res;
 };
-
-snail([
-    [1, 2, 3, 4, 5],
-    [6, 7, 8, 9, 10],
-    [11, 12, 13, 14, 15],
-    [16, 17, 18, 19, 20],
-    [21, 22, 23, 24, 25],
-]);
-
-// [1, 2, 3, 4, 5, 10, 15, 20, 25, 24, 23, 22, 21, 16, 11, 6, 7, 8, 9, 14, 19, 18, 17, 12, 13];
