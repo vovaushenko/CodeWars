6a5a4520e1c857c7f495d4d2d5be8b6b80cb9da0
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-01-14 14:03:28 +0200

    7 kyu Slope of a Line

--- 7 kyu Slope of a Line.js
+++ 7 kyu Slope of a Line.js
@@ -1,5 +1,3 @@
 // https://www.codewars.com/kata/53222010db0eea35ad000001
 
 const getSlope = (p1, p2) => (p2[0] == p1[0] ? null : (p2[1] - p1[1]) / (p2[0] - p1[0]));
-
-console.log(getSlope([1, 1], [1, 2]));
