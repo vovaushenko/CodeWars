18cbe2899685529bc3350343dbf7e92d9b2dc92c
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-01-15 10:14:19 +0200

    6 kyu String Breakers

--- 6 kyu String Breakers.js
+++ 6 kyu String Breakers.js
@@ -1,10 +1,5 @@
 // https://www.codewars.com/kata/59d398bb86a6fdf100000031
 
-// 1) get rid of spaces
-// 2) recursion
-// 3) slice (0,5)
-// 4) push to result
-
 const stringBreakers = (n, input) => {
     let noWhiteSpaces = input.replace(/\s/g, '');
     let result = [];
@@ -20,5 +15,3 @@ const stringBreakers = (n, input) => {
 
     return result.join('\n');
 };
-
-stringBreakers(5, 'This is an example string');
