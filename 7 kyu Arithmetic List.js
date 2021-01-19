b7dc91a5edd883542ecc31ade3442d558afc8b0a
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-01-19 15:49:55 +0200

    7 kyu Arithmetic List!

--- 7 kyu Arithmetic List.js
+++ 7 kyu Arithmetic List.js
@@ -1,5 +1,3 @@
 // https://www.codewars.com/kata/541da001259d9ca85d000688
 
 const seqlist = (first, c, l) => Array.from({ length: l }, (_, id) => first + id * c);
-
-console.log(seqlist(0, 1, 20));
