0f2b0865ee74acd0721654c2e0c60df8e1113aab
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-01-19 16:07:04 +0200

    7 kyu makeAcronym

--- app.js
+++ app.js
@@ -1,22 +1,13 @@
-// https://www.codewars.com/kata/5572392fee5b0180480001ae
+// https://www.codewars.com/kata/557efeb04effce569d000022
 
-const computerToPhone = (nums) => {
-  const PC_TO_PHONE = {
-    1: 7,
-    2: 8,
-    3: 9,
-    4: 4,
-    5: 5,
-    6: 6,
-    7: 1,
-    8: 2,
-    9: 3,
-    0: 0,
-  };
+const makeAcronym = (str) => {
+  if (!str) return '';
+  if (typeof str !== 'string') return 'Not a string';
+  if (str.replace(/\D+/g, '')) return 'Not letters';
 
-  let result = [...nums].map((digit) => PC_TO_PHONE[digit]).join('');
-
-  return result;
+  return str
+    .split(' ')
+    .map((word) => word[0].toUpperCase())
+    .join('');
 };
-
-computerToPhone('0789456123');
+console.log(makeAcronym('hey there'));
