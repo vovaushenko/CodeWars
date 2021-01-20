e056356b5f6ec3199509bd55905d60c11e1c030d
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-01-20 13:51:09 +0200

    6 kyu Once

--- 6 kyu Once.js
+++ 6 kyu Once.js
@@ -1,13 +1,3 @@
-function once(func) {
-  let called = false;
-  return function (...arguments) {
-    if (!called) {
-      called = true;
-      return func.apply(this, arguments);
-    }
-  };
-}
-
 const once = (passedFunction) => {
   let wasCalled = false;
 
