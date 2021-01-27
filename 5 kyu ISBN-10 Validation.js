ec9a7f202c1bf2f8e5b46faa08c435145706b366
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-01-27 17:01:05 +0200

    5 kyu ISBN-10 Validation

--- 5 kyu ISBN-10 Validation.js
+++ 5 kyu ISBN-10 Validation.js
@@ -4,5 +4,3 @@ const validISBN10 = (isbn) =>
   isbn.length !== 10 || isbn.slice(0, -1).includes('X')
     ? false
     : [...isbn].reduce((acc, digit, id) => acc + (digit === 'X' ? 10 : digit) * ++id, 0) % 11 === 0;
-
-console.log(validISBN10('XXXXXXXXXX'));
