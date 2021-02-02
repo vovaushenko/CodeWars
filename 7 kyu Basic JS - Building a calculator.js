4f61fd4ca01b57d329e5742fefd59a27a606f881
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-02-02 20:47:47 +0200

    Codewars ...

---
+++ 7 kyu Basic JS - Building a calculator.js
@@ -0,0 +1,4 @@
+Calculator.add = (a, b) => a + b;
+Calculator.subtract = (a, b) => a - b;
+Calculator.multiply = (a, b) => a * b;
+Calculator.divide = (a, b) => (b !== 0 ? a / b : false);
--- app.js
+++
@@ -1,3 +0,0 @@
-// https://www.codewars.com/kata/5220b7f77e831a7e79000417
-
-const generateRules = (func, porlist) => porlist.map((port) => func(port)).join('');
