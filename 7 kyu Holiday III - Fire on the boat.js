cb1a29f41088cdd052a0467484d4f320ae3ea237
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-02-03 18:19:07 +0200

    7 kyu Holiday III - Fire on the boat

--- 7 kyu Holiday III - Fire on the boat.js
+++ 7 kyu Holiday III - Fire on the boat.js
@@ -1,5 +1,3 @@
 // https://www.codewars.com/kata/57e8fba2f11c647abc000944
 
 const fireFight = (str) => str.replace(new RegExp('Fire', 'g'), '~~');
-
-console.log(fireFight('Mast FireDeck Engine Water Fire'));
