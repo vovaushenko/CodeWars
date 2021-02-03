7e9045826b2a04ac2dbf735f33893f477605a180
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-02-03 19:43:28 +0200

    6 kyu Time-like string format

--- 6 kyu Time-like string format.js
+++ 6 kyu Time-like string format.js
@@ -1,8 +1,5 @@
 // https://www.codewars.com/kata/51e000d070fe4414000003f0
 
-// @step 1 -> num to array
-// @step 2 -> work with exceptions (length < 3 length > 4)
-//@ step 3 ->
 const solution = (num) => {
   let arr = [...(num + '')];
   if (arr.length < 3 || arr.length > 4) throw new Error('Length is invalid');
@@ -10,5 +7,3 @@ const solution = (num) => {
   arr.splice(~~arr.length / 2, 0, ':');
   return arr.join('');
 };
-
-const solution = (num) => console.log(solution(3314));
