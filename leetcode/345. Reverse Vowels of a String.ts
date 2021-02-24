24258fe4db33bb03a3d85de5dfa2bbbcbd6677a2
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-02-24 15:03:05 +0200

    345. Reverse Vowels of a String

--- leetcode/345. Reverse Vowels of a String.ts
+++ leetcode/345. Reverse Vowels of a String.ts
@@ -1,20 +1,24 @@
 // 345. Reverse Vowels of a String
 // Write a function that takes a string as input and reverse only the vowels of a string.
 
+// swap function, to swap two values in array
 const swap = (a: string[], id1: number, id2: number) => ([a[id1], a[id2]] = [a[id2], a[id1]]);
 
 const reverseVowels = (str: string): string => {
+  //convert string into array for further swapping
   const arr = str.split('');
   const vowels = 'aeiouAEIOU';
 
+  //store indexes of vowels in swapIds array
   const swapIds = [];
   for (let i = 0; i < arr.length; i++) {
     if (vowels.includes(arr[i])) swapIds.push(i);
   }
 
+  //use to pointers in indexes array
   let k = 0;
   let j = swapIds.length - 1;
-
+  //iterate through id-array and swap vowels in our initial arr
   while (k < j) {
     swap(arr, swapIds[k], swapIds[j]);
     k++;
@@ -23,6 +27,3 @@ const reverseVowels = (str: string): string => {
 
   return arr.join('');
 };
-console.log(reverseVowels('aA'));
-// "hello"
-// i    j
