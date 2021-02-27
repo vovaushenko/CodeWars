ab91a92191e0ac41b0a5a4d35ca222b57976ff13
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-02-27 19:59:51 +0200

    1470. Shuffle the Array

--- leetcode/1470. Shuffle the Array.ts
+++ leetcode/1470. Shuffle the Array.ts
@@ -1,4 +1,7 @@
 // 1470. Shuffle the Array
+// Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+
+// Return the array in the form [x1,y1,x2,y2,...,xn,yn].
 
 function shuffle(nums: number[], n: number): number[] {
   const result = [];
@@ -9,5 +12,3 @@ function shuffle(nums: number[], n: number): number[] {
 
   return result;
 }
-
-shuffle([1, 2, 3, 4, 4, 3, 2, 1], 4);
