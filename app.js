ec19bedb3768e73853ad2b8aa155210b53f953ef
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-03-01 08:52:37 +0200

    3 kyu The Millionth Fibonacci Kata

--- app.js
+++ app.js
@@ -1,25 +1,63 @@
-// https://www.codewars.com/kata/52ab60b122e82a6375000bad
+// Dynamic programming
 
-const sortReindeer = (reindeerNames) => {
-  reindeerNames.sort((a, b) => {
-    const [_, lastName1] = a.split(' ');
-    const [__, lastName2] = b.split(' ');
+// const basicFib = (n) => {
+//   if (n <= 2) return 1;
 
-    return lastName1.localeCompare(lastName2);
-  });
+//   return basicFib(n - 2) + basicFib(n - 1);
+// };
 
-  return reindeerNames;
+// const fib = (n, memo = [0n, 1n]) => {
+//   if (n < 0) {
+//     n = Math.abs(n);
+//     if (memo[n] !== undefined) return memo[n];
+
+//     let res = fib(n - 2, memo) + fib(n - 1, memo);
+
+//     memo[n] = res;
+//     return n % 2 === 0 ? -res : res;
+//   }
+
+//   if (memo[n] !== undefined) return memo[n];
+
+//   let res = fib(n - 2, memo) + fib(n - 1, memo);
+
+//   memo[n] = res;
+
+//   return res;
+// };
+
+// const tabulatedFibs = (n) => {
+//   const memo = [0n, 1n];
+
+//   while (memo.length <= Math.abs(n)) {
+//     memo.push(memo[memo.length - 2] + memo[memo.length - 1]);
+//   }
+
+//   if (n < 0) {
+//     return n % 2 === 0 ? -memo[Math.abs(n)] : memo[Math.abs(n)];
+//   }
+
+//   return memo[Math.abs(n)];
+// };
+
+const fib = (n) => {
+  const memo = {
+    0: 0n,
+    1: 1n,
+  };
+
+  let counter = 2;
+
+  while (counter <= Math.abs(n)) {
+    memo[counter] = memo[counter - 1] + memo[counter - 2];
+    counter++;
+  }
+
+  if (n < 0) {
+    return n % 2 === 0 ? -memo[Math.abs(n)] : memo[Math.abs(n)];
+  }
+
+  return memo[Math.abs(n)];
 };
 
-console.log(
-  sortReindeer([
-    'Dasher Tonoyan',
-    'Dancer Moore',
-    'Prancer Chua',
-    'Vixen Hall',
-    'Comet Karavani',
-    'Cupid Foroutan',
-    'Donder Jonker',
-    'Blitzen Claus',
-  ])
-);
+console.log(memoFibs(-6));
