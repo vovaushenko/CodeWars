404e489b53fe1fb99f03ff4b5720ba502ba8cd1e
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-03-03 16:25:57 +0200

    1122. Relative Sort Array

--- leetcode/1122. Relative Sort Array.ts
+++ leetcode/1122. Relative Sort Array.ts
@@ -22,5 +22,3 @@ const relativeSortArray = (arr1: number[], arr2: number[]): number[] => {
 
   return res.concat(nonAppearing.sort((a, b) => a - b));
 };
-
-console.log(relativeSortArray([2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19], [2, 1, 4, 3, 9, 6]));
