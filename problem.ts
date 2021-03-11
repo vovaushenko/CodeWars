0f1641ffcb3aa6efc75f0db67f551b7777af2daf
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-03-11 21:02:58 +0200

    1295. Find Numbers with Even Number of Digits

--- problem.ts
+++ problem.ts
@@ -1,7 +1,10 @@
 // 1295. Find Numbers with Even Number of Digits
 // Given an array nums of integers, return how many of them contain an even number of digits.
 
-const findNumbers = (nums: number[]): any =>
-  nums.map((digit) => digit + '').filter((digit) => !(digit.length % 2)).length;
+const findNumbers = (nums: number[]): number =>
+  nums.reduce(
+    (evenCounter, digit) => (!(String(digit).length % 2) ? evenCounter + 1 : evenCounter),
+    0
+  );
 
-console.log(findNumbers([12, 345, 2, 6, 7896]));
+console.log(findNumbers([1, 2, 3, 44, 55, 66, 66]));
