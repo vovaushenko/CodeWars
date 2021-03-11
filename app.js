1f40fa9e8518dc664a96449fade980241d8b50e4
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-03-11 13:45:26 +0200

    278. First Bad Version

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-//www.codewars.com/kata/57d81d428462e10e63000205
+function solution(isBadVersion) {
+  return function (n) {
+    var left = 0;
+    var right = n;
 
-const fibonacci = (max, memo = [0, 1]) => {
-  let evenSum = 0;
-  while (memo[memo.length - 1] + memo[memo.length - 2] < max) {
-    memo.push(memo[memo.length - 1] + memo[memo.length - 2]);
-    if (memo[memo.length - 1] % 2 === 0) evenSum += memo[memo.length - 1];
-  }
-  return evenSum;
-};
+    while (right - left !== 1) {
+      var mid = parseInt((left + right) / 2);
 
-console.log(fibonacci(25997544));
+      if (isBadVersion(mid)) {
+        right = mid;
+      } else {
+        left = mid;
+      }
+    }
+    return right;
+  };
+}
