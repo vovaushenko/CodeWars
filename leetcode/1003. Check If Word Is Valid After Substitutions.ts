7eab7e87ee286baabc0cff2d807d4a45f937d87b
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-03-14 21:31:46 +0200

    1003. Check If Word Is Valid After Substitutions

---
+++ leetcode/1003. Check If Word Is Valid After Substitutions.ts
@@ -0,0 +1,17 @@
+// 1003. Check If Word Is Valid After Substitutions
+
+// Given a string s, determine if it is valid.
+
+// A string s is valid if, starting with an empty string t = "", you can transform t into s after performing the following operation any number of times:
+
+// Insert string "abc" into any position in t. More formally, t becomes tleft + "abc" + tright, where t == tleft + tright. Note that tleft and tright may be empty.
+// Return true if s is a valid string, otherwise, return false.
+
+function isValid(s: string): boolean {
+  while (s.length) {
+    if (s === s.replace(/abc/g, '')) return false;
+    s = s.replace(/abc/g, '');
+  }
+
+  return true;
+}
