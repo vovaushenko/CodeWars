2ade43776346963ea4c0c4551cc178ffe0880321
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-03-18 11:10:17 +0200

    6 kyu Coding Meetup #8 - Higher-Order Functions Series - Will all continents be represented

--- Coding Meetup #8 - Higher-Order Functions Series - Will all continents be represented.js
+++ Coding Meetup #8 - Higher-Order Functions Series - Will all continents be represented.js
@@ -1,5 +1,3 @@
 // https://www.codewars.com/kata/coding-meetup-number-8-higher-order-functions-series-will-all-continents-be-represented
 
-// 2
-
 const allContinents = (list) => [...new Set(list.map((dev) => dev.continent))].length === 5;
