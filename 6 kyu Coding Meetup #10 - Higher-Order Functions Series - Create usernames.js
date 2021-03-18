90bddf87c674cb5b486d49840fdc3b1cbe1751f8
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-03-18 14:13:56 +0200

    6 kyu Coding Meetup #10 - Higher-Order Functions Series - Create usernames

--- 6 kyu Coding Meetup #10 - Higher-Order Functions Series - Create usernames.js
+++ 6 kyu Coding Meetup #10 - Higher-Order Functions Series - Create usernames.js
@@ -1,24 +1,5 @@
 // https://www.codewars.com/kata/coding-meetup-number-10-higher-order-functions-series-create-usernames
 
-var list1 = [
-  {
-    firstName: 'Emily',
-    lastName: 'N.',
-    country: 'Ireland',
-    continent: 'Europe',
-    age: 30,
-    language: 'Ruby',
-  },
-  {
-    firstName: 'Nor',
-    lastName: 'E.',
-    country: 'Malaysia',
-    continent: 'Asia',
-    age: 20,
-    language: 'Clojure',
-  },
-];
-
 const addUsername = (list) => {
   for (let developer of list) {
     const { firstName, lastName, age } = developer;
@@ -29,5 +10,3 @@ const addUsername = (list) => {
 
   return list;
 };
-
-addUsername(list1);
