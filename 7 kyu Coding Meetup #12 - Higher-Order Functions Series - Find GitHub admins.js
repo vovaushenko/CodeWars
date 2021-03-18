5c2b1c1f219f29c2885f451b8df0746e978d96e5
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-03-18 14:19:53 +0200

    7 kyu Coding Meetup #12 - Higher-Order Functions Series - Find GitHub admins

--- 7 kyu Coding Meetup #12 - Higher-Order Functions Series - Find GitHub admins.js
+++ 7 kyu Coding Meetup #12 - Higher-Order Functions Series - Find GitHub admins.js
@@ -1,5 +1,4 @@
 // https://www.codewars.com/kata/coding-meetup-number-12-higher-order-functions-series-find-github-admins
 
-// 3
 const findAdmin = (list, lang) =>
   list.filter((dev) => dev.language === lang && dev.githubAdmin === 'yes');
