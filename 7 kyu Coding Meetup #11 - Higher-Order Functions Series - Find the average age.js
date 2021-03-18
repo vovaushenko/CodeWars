0543c45d3a238e8a7f6d93c61f5d4af04ce2b880
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-03-18 14:16:25 +0200

    7 kyu Coding Meetup #11 - Higher-Order Functions Series - Find the average age

--- 7 kyu Coding Meetup #11 - Higher-Order Functions Series - Find the average age.js
+++ 7 kyu Coding Meetup #11 - Higher-Order Functions Series - Find the average age.js
@@ -1,5 +1,3 @@
 // https://www.codewars.com/kata/coding-meetup-number-11-higher-order-functions-series-find-the-average-age
 
-// 1
-
 const getAverageAge = (list) => Math.round(list.reduce((s, dev) => s + dev.age, 0) / list.length);
