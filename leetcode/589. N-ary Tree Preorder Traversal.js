76a42d443ad898d6f07f4d73714366bc03cd9a32
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-03-18 18:53:32 +0200

    589. N-ary Tree Preorder Traversal

--- leetcode/589. N-ary Tree Preorder Traversal.js
+++ leetcode/589. N-ary Tree Preorder Traversal.js
@@ -15,15 +15,3 @@ const preorder = (root) => {
 
   return values;
 };
-
-let preorder = (root, ans = []) => {
-  let queue = [root];
-  while (queue.length > 0) {
-    let cur = queue[0];
-    queue.shift();
-    if (!cur) continue;
-    ans.push(cur.val);
-    queue.unshift(...cur.children);
-  }
-  return ans;
-};
