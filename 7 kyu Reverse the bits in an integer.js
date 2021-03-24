a8f825e5fe62dfc928f9534d7ca7867e9d9d7665
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-03-24 15:52:05 +0200

    7 kyu Reverse the bits in an integer

--- 7 kyu Reverse the bits in an integer.js
+++ 7 kyu Reverse the bits in an integer.js
@@ -1,5 +1,3 @@
 // https://www.codewars.com/kata/5959ec605595565f5c00002b
 
 const reverseBits = (n) => parseInt([...n.toString(2)].reverse().join(''), 2);
-
-console.log(reverseBits(6090250174530944));
