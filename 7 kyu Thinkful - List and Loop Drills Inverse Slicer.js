cdbd337accd00c3a1c0b9d50bf28fe823aec9230
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-03-25 10:38:42 +0200

    7 kyu Thinkful - List and Loop Drills: Inverse Slicer

--- 7 kyu Thinkful - List and Loop Drills Inverse Slicer.js
+++ 7 kyu Thinkful - List and Loop Drills Inverse Slicer.js
@@ -1,5 +1,3 @@
 // https://www.codewars.com/kata/586ec0b8d098206cce001141
 
 const inverseSlice = (nums, a, b) => nums.filter((_, id) => id < a || id >= b);
-
-console.log(inverseSlice([12, 14, 63, 72, 55, 24], 0, 3));
