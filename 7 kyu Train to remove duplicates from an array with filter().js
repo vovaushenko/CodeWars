95b1da464f8e281542dbd74558b167bb0a107b3a
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-03-28 16:08:23 +0300

    6 kyu Duplicates. Duplicates Everywhere.

---
+++ 7 kyu Train to remove duplicates from an array with filter().js
@@ -0,0 +1,3 @@
+// https://www.codewars.com/kata/58308360aeb69a460b0002b2
+
+const unique = (arr) => arr.filter((num, id) => arr.indexOf(num) === id);
--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-// https://www.codewars.com/kata/5904d222e1bfd3354e000026
+// https://www.codewars.com/kata/5e8dd197c122f6001a8637ca
 
-const removeDuplication = (arr) =>
-  Object.entries(arr.reduce((h, c) => ((h[c] = h[c] + 1 || 1), h), {}))
-    .filter(([_, frequency]) => frequency === 1)
-    .map(([num, _]) => +num);
+const removeDuplicateIds = (obj) => {
+  let uniqueValues = [...new Set(Object.values(obj).reduce((t, v) => t.concat(v), []))];
+  let indexes = Object.keys(obj);
+  console.log(uniqueValues);
 
-console.log(removeDuplication([1, 2, 3, 2, 4, 6, 2, 6, 7]));
+  let uniqueObj = {};
+
+  for (let i = indexes.length - 1; i >= 0; i--) {
+    let index = indexes[i];
+
+    let values = uniqueValues.filter((val) => obj[index].includes(val));
+    uniqueValues = uniqueValues.filter((val) => !obj[index].includes(val));
+    uniqueObj[index] = values.sort();
+  }
+
+  return uniqueObj;
+};
+console.log(
+  removeDuplicateIds({
+    432: ['A', 'A', 'B', 'D'],
+    53: ['L', 'G', 'B', 'C'],
+    236: ['L', 'A', 'X', 'G', 'H', 'X'],
+    11: ['P', 'R', 'S', 'D'],
+  })
+);
