3eacfbd42cbc8af90d7a636fa02febfb5549d00b
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-03-29 11:01:16 +0300

    7 kyu The Office I - Outed

--- 7 kyu The Office I - Outed.js
+++ 7 kyu The Office I - Outed.js
@@ -8,22 +8,3 @@ const outed = (meet, boss) => {
 
   return totalHappiness / length <= 5 ? 'Get Out Now!' : 'Nice Work Champ!';
 };
-
-console.log(
-  outed(
-    {
-      tim: 2,
-      jim: 4,
-      randy: 0,
-      sandy: 5,
-      andy: 8,
-      katie: 6,
-      laura: 2,
-      saajid: 2,
-      alex: 3,
-      john: 2,
-      mr: 8,
-    },
-    'john'
-  )
-);
