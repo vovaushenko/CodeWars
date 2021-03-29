d557acf2663a8c5cfc27a64832df9329ec358b95
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-03-29 10:50:49 +0300

    7 kyu Calculate Two People's Individual Ages

--- 7 kyu Calculate Two People's Individual Ages.js
+++ 7 kyu Calculate Two People's Individual Ages.js
@@ -1,7 +1,8 @@
+// https://www.codewars.com/kata/58e0bd6a79716b7fcf0013b1
+
 const getAges = (sum, difference) => {
   let minAge = (sum - difference) / 2;
   let maxAge = sum - minAge;
   if (minAge < 0 || maxAge < 0) return null;
   return [minAge, maxAge];
 };
-sum < 0 || difference < 0 ? null : [(sum - difference) / 2 + difference, (sum - difference) / 2];
