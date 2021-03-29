206621dd3ad2e4abbad1c3a989d69fef1d01be1c
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-03-29 09:29:42 +0300

    7 kyu Array Mash

--- 7 kyu Array Mash.js
+++ 7 kyu Array Mash.js
@@ -1,5 +1,3 @@
 // https://www.codewars.com/kata/582642b1083e12521f0000da/train/javascript
 
 const arrayMash = (a1, a2) => a1.reduce((mash, digit, id) => mash.concat(digit, a2[id]), []);
-
-console.log(arrayMash([1, 2, 3], ['a', 'b', 'c']));
