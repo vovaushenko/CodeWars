b092ebb3a0cb426e6bced377d9c5e1a514b5355d
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-03-29 11:08:12 +0300

    7 kyu Working with arrays I (and why your code fails in some katas)

--- 7 kyu Working with arrays I (and why your code fails in some katas).js
+++ 7 kyu Working with arrays I (and why your code fails in some katas).js
@@ -1,5 +1,3 @@
 // https://www.codewars.com/kata/5a4ff3c5fd56cbaf9800003e/train/javascript
 
 const withoutLast = (arr) => arr.slice(0, arr.length - 1);
-
-console.log(withoutLast([1, 2, 3, 4, 5]));
