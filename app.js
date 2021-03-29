e3535008b6c1d50c3a96df8822016de0b19cb8f5
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-03-29 13:17:25 +0300

    7 kyu Sorting Dictionaries

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-// https://www.codewars.com/kata/563b1f55a5f2079dc100008a
+// https://www.codewars.com/kata/53da6a7e112bd15cbc000012
 
-const getLargerNumbers = (a1, a2) =>
-  a1.reduce((largest, num, id) => largest.concat(Math.max(num, a2[id])), []);
-
-getLargerNumbers([13, 64, 15, 17, 88], [23, 14, 53, 17, 80]);
+const sortDict = (dict) =>
+  Object.entries(dict)
+    .sort((a, b) => b[1] - a[1])
+    .map(([a, b]) => [+a ? +a : a, b]);
