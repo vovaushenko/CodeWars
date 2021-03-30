24cf5de700ce5cce140048fc2cae4539779fa247
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-03-30 09:51:23 +0300

    7 kyu Are they square?

--- 7 kyu Are they square.js
+++ 7 kyu Are they square.js
@@ -1,7 +1,3 @@
 // https://www.codewars.com/kata/56853c44b295170b73000007
 
 const isSquare = (arr) => (arr.length ? arr.every((num) => !(Math.sqrt(num) % 1)) : undefined);
-
-console.log(isSquare([1, 4, 9, 16]));
-
-console.log(Math.sqrt(16) % 1);
