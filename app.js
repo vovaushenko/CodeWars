cccc723f2014ea9881231bcb5f14c79d0672a240
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-04-02 09:59:24 +0300

    7 kyu Playing with Sets : Intersection

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 // https://www.codewars.com/kata/5a662a02e626c54e87000123
 
-const isPerfect = (n) =>
-  [...n.toString(2)]
-    .filter((_, id) => id === 0 || id === [...n.toString(2)].length - 1)
-    .reduce((a, b) => a + +b, 0) === 2;
+const inter = (s1, s2) => {
+  const intersection = new Set();
+  for (let val of s1) {
+    if (s2.has(val)) intersection.add(val);
+  }
 
-const extraPerfect = (n) =>
-  Array.from({ length: n }, (_, id) => id + 1).filter((num) => isPerfect(num) || num === 1);
+  return intersection;
+};
 
-console.log(extraPerfect(7));
+console.log(inter(A, B));
