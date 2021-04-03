6746d04c1a2633ad7b9afee55de499e7052fefbd
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-04-03 14:50:19 +0300

    7 kyu Change two-dimensional array

--- 7 kyu Change two-dimensional array.js
+++ 7 kyu Change two-dimensional array.js
@@ -2,13 +2,3 @@
 
 const matrix = (matrix) =>
   matrix.map((row, id) => row.map((el, secondId) => (id === secondId ? (el < 0 ? 0 : 1) : el)));
-
-console.log(
-  matrix([
-    [-1, 4, -5, -9, 3],
-    [6, -4, -7, 4, -5],
-    [3, 5, 0, -9, -1],
-    [1, 5, -7, -8, -9],
-    [-3, 2, 1, -5, 6],
-  ])
-);
