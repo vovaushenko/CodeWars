1f3f2d564c9c85cb73aac29afa55883bd5973de6
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-04-04 09:39:17 +0300

    7 kyu Identical Elements

--- 7 kyu Identical Elements.js
+++ 7 kyu Identical Elements.js
@@ -1,4 +1,2 @@
 // https://www.codewars.com/kata/583ebb9328a0c034490001ba
 const duplicateElements = (arr1, arr2) => arr1.filter((num) => arr2.includes(num)).length > 0;
-
-console.log(duplicateElements([1, 2, 3, 4, 5], [1, 6, 7, 8, 9]));
