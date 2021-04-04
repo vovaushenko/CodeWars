df465ea05607a4969a1b84ee2bd78efb2f50885a
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-04-04 14:08:54 +0300

    7 kyu Friday the 13th Part 1

--- 7 kyu Friday the 13th Part 1.js
+++ 7 kyu Friday the 13th Part 1.js
@@ -1,18 +1,6 @@
-// 1
-
 // https://www.codewars.com/kata/5925acf31a9825d616000e74
 
 const killcount = (counselors, jason) =>
   counselors
     .filter(([_, intelligence]) => intelligence < jason)
     .reduce((res, [name, _]) => res.concat(name), []);
-
-console.log(
-  killcount(
-    [
-      ['Mike', 7],
-      ['Alysa', 3],
-    ],
-    7
-  )
-);
