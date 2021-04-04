4beb11c467c0906579bb851624671dac866a29d5
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-04-04 18:12:14 +0300

    7 kyu Sub-array elements sum

--- 7 kyu Sub-array elements sum.js
+++ 7 kyu Sub-array elements sum.js
@@ -4,13 +4,3 @@ const elementsSum = (arr, d = 0) =>
       subarr[arr.length - id - 1] !== undefined ? subarr[arr.length - id - 1] : d
     )
     .reduce((a, b) => a + b, 0);
-
-console.log(
-  elementsSum(
-    [
-      [4, 0],
-      [0, 0],
-    ],
-    2
-  )
-);
