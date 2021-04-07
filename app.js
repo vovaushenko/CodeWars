62733294d6cc39407092ab1b29315c1e159c185b
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-04-07 18:21:43 +0300

    1742. Maximum Number of Balls in a Box

--- app.js
+++ app.js
@@ -1,27 +1,19 @@
-// 1189. Maximum Number of Balloons
-// Given a string text, you want to use the characters of text to form as many instances of the word "balloon" as possible.
+// 1742. Maximum Number of Balls in a Box
 
-// You can use each character in text at most once. Return the maximum number of instances that can be formed.
+const countBalls = (lowLimit, highLimit) => {
+  const boxes = {};
 
-const maxNumberOfBalloons = (str) => {
-  const balooonMap = [...str]
-    .filter((char) => 'balloon'.includes(char))
-    .sort()
-    .reduce((h, c) => ((h[c] = h[c] + 1 || 1), h), {});
-
-  for (let char of 'balon') {
-    if (!(char in balooonMap)) return 0;
+  for (let i = lowLimit; i <= highLimit; i++) {
+    if (!boxes[sumOfDigits(i)]) {
+      boxes[sumOfDigits(i)] = 1;
+    } else {
+      boxes[sumOfDigits(i)]++;
+    }
   }
 
-  if (balooonMap['l'] < 2 || balooonMap['o'] < 2) return 0;
-  balooonMap['l'] = Math.floor(balooonMap['l'] / 2);
-  balooonMap['o'] = Math.floor(balooonMap['o'] / 2);
-
-  return Math.min(...Object.values(balooonMap));
+  return Math.max(...Object.values(boxes));
 };
 
-console.log(
-  maxNumberOfBalloons(
-    'krhizmmgmcrecekgyljqkldocicziihtgpqwbticmvuyznragqoyrukzopfmjhjjxemsxmrsxuqmnkrzhgvtgdgtykhcglurvppvcwhrhrjoislonvvglhdciilduvuiebmffaagxerjeewmtcwmhmtwlxtvlbocczlrppmpjbpnifqtlninyzjtmazxdbzwxthpvrfulvrspycqcghuopjirzoeuqhetnbrcdakilzmklxwudxxhwilasbjjhhfgghogqoofsufysmcqeilaivtmfziumjloewbkjvaahsaaggteppqyuoylgpbdwqubaalfwcqrjeycjbbpifjbpigjdnnswocusuprydgrtxuaojeriigwumlovafxnpibjopjfqzrwemoinmptxddgcszmfprdrichjeqcvikynzigleaajcysusqasqadjemgnyvmzmbcfrttrzonwafrnedglhpudovigwvpimttiketopkvqw'
-  )
-);
+const sumOfDigits = (num) => +[...(num + '')].reduce((a, b) => a + +b, 0);
+
+console.log(countBalls(5, 15));
