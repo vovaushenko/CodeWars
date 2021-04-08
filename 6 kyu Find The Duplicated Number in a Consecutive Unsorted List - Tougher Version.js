975ae57ed4ff69c7153677279cde631152bca5b4
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-04-08 09:30:23 +0300

    6 kyu Find The Duplicated Number in a Consecutive Unsorted List - Tougher Version

--- 6 kyu Find The Duplicated Number in a Consecutive Unsorted List - Tougher Version.js
+++ 6 kyu Find The Duplicated Number in a Consecutive Unsorted List - Tougher Version.js
@@ -4,5 +4,3 @@ const findDup = (arr) =>
   Object.entries(arr.reduce((h, c) => ((h[c] = h[c] + 1 || 1), h), {})).find(
     ([_, frequency]) => frequency === 2
   )[0] * 1;
-
-console.log(findDup([1, 2, 3, 1]));
