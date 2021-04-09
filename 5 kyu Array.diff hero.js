7525fbffe3ee42692799ad903b712aa19cefc9c3
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-04-09 13:20:17 +0300

    5 kyu Array.diff hero

--- 5 kyu Array.diff hero.js
+++ 5 kyu Array.diff hero.js
@@ -4,5 +4,3 @@ const array_diff_very_fast = (a, b) => {
   let mapB = b.reduce((h, c) => ((h[c] = h[c] + 1 || 1), h), {});
   return a.filter((char) => !(char in b.reduce((h, c) => ((h[c] = h[c] + 1 || 1), h), {})));
 };
-
-console.log(array_diff_very_fast([1, 2, 2, 2, 3], [2]));
