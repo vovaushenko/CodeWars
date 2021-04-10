4206e469f77b787513f94f221d84c9e825077824
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-04-10 14:46:46 +0300

    7 kyu Drone Fly-By

---
+++ 7 kyu Drone Fly-By.js
@@ -0,0 +1,4 @@
+// https://www.codewars.com/kata/58356a94f8358058f30004b5
+
+const flyBy = (lamps, drone) =>
+  'o'.repeat(drone.length < lamps.length ? drone.length : lamps.length) + lamps.slice(drone.length);
