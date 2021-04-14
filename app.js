db13b86a86317a0bd323aed0a6feeceb421db553
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-04-14 14:44:30 +0300

    7 kyu Selective fear of numbers

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-// https://www.codewars.com/kata/559011063089b0d5500001aa
-
-const CalculateString = (_, nums) =>
-  Math.abs(
-    [...nums].filter((num) => num === '0').length - [...nums].filter((num) => num === '1').length
-  );
+var AmIAfraid = function (day, num) {
+  var preds = {
+    Sunday: (d) => d == 666 || d == -666,
+    Monday: (d) => d == 12,
+    Tuesday: (d) => d > 95,
+    Wednesday: (d) => d == 34,
+    Thursday: (d) => d === 0,
+    Friday: (d) => d % 2 === 0,
+    Saturday: (d) => d == 56,
+  };
+  return preds[day](num);
+};
