813b31149500149eb6d87d8029a12a52443dcdad
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-04-17 13:44:27 +0300

    6 kyu "this" is an other solution (JavaScript)

--- 6 kyu this is an other solution (JavaScript).js
+++ 6 kyu this is an other solution (JavaScript).js
@@ -15,10 +15,3 @@ class OnceNamedOne {
     return `${this.firstName} ${this.lastName}`;
   }
 }
-
-let nameOne = new OnceNamedOne('Naomi', 'Wang');
-console.log(nameOne);
-
-nameOne.firstName = 'Vasia';
-console.log(nameOne);
-console.log(nameOne.fullName);
