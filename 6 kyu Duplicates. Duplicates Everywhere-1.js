106ab69a013b888d1224775025d8243e697fd7d2
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-04-17 16:44:36 +0300

    6 kyu Duplicates. Duplicates Everywhere.

--- 6 kyu Duplicates. Duplicates Everywhere-1.js
+++ 6 kyu Duplicates. Duplicates Everywhere-1.js
@@ -14,10 +14,3 @@ const removeDuplicateIds = (obj) => {
 
   return res;
 };
-
-removeDuplicateIds({
-  432: ['A', 'A', 'B', 'D'],
-  53: ['L', 'G', 'B', 'C'],
-  236: ['L', 'A', 'X', 'G', 'H', 'X'],
-  11: ['P', 'R', 'S', 'D'],
-});
