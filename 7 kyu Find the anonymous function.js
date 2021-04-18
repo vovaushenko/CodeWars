c73a777ea2e94f27ccb84e4f2c39a922adf0c8af
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-04-18 09:17:10 +0300

    7 kyu Find the anonymous function

--- 7 kyu Find the anonymous function.js
+++ 7 kyu Find the anonymous function.js
@@ -2,5 +2,3 @@
 
 const FindFunction = (arr1, arr2) =>
   arr2.filter((el) => arr1.find((e) => typeof e === 'function')(el));
-
-console.log(FindFunction([(a) => a % 2 == 0, 9, 3, 1, 0], [1, 2, 3, 4]));
