e2224e71d0c55f338ea8f6a7cf965032f33c64c0
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-04-25 11:04:39 +0300

    7 kyu Hamming Distance - Part 1: Binary codes

--- 7 kyu Hamming Distance - Part 1 Binary codes.js
+++ 7 kyu Hamming Distance - Part 1 Binary codes.js
@@ -1,5 +1,3 @@
 // https://www.codewars.com/kata/5624e574ec6034c3a20000e6
 
 const hammingDistance = (s1, s2) => [...s1].filter((bit, id) => bit !== s2[id]).length;
-
-console.log(hammingDistance('1010', '0101'));
