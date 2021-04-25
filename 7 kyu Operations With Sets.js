7a7b9b7356082622910b8790964762ce2457a68f
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-04-25 11:02:02 +0300

    7 kyu Operations With Sets

--- 7 kyu Operations With Sets.js
+++ 7 kyu Operations With Sets.js
@@ -6,5 +6,3 @@ const process2Arrays = (a1, a2, intersection = a1.filter((num) => a2.includes(nu
   a1.length - intersection,
   a2.length - intersection,
 ];
-
-console.log(process2Arrays([1, 2, 3, 4, 5, 6, 7, 8, 9], [2, 4, 6, 8, 10, 12, 14]));
