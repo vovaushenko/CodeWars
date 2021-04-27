18ec5304744c511e3cd3936311c7ad65bad70d80
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-04-27 10:20:25 +0300

    7 kyu Basic Math (Add or Subtract)

--- 7 kyu Basic Math (Add or Subtract).js
+++ 7 kyu Basic Math (Add or Subtract).js
@@ -1,5 +1,3 @@
 // https://www.codewars.com/kata/5809b62808ad92e31b000031
 
 const calculate = (s) => String(eval(s.replace(/(plus)/gi, '+').replace(/(minus)/gi, '-')));
-
-console.log(calculate('1plus2plus3minus4'));
