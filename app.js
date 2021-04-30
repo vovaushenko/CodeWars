19c5b6b526e0e7f6a1d2dec87b552b74e8c15fdd
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-04-30 20:38:42 +0300

    657. Robot Return to Origin

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-// https://www.codewars.com/kata/567d71b93f8a50f461000019
+// 657. Robot Return to Origin
 
-const crossover = (c1, c2, index) => {
-	return [
-		c1.slice(0, index) + c2.slice(index),
-		c2.slice(0, index) + c1.slice(index),
-	];
-};
+const judgeCircle = (moves) =>
+	Object.values(
+		[...moves].reduce(
+			(coords, move) => {
+				if ('LR'.includes(move)) move === 'R' ? coords.x++ : coords.x--;
+				if ('UD'.includes(move)) move === 'U' ? coords.y++ : coords.y--;
+				return coords;
+			},
+			{ x: 0, y: 0 }
+		)
+	).every((coord) => coord === 0);
+
+console.log(judgeCircle('RRDD'));
