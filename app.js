4015764093666b6fd659bcad31ec1aadf44eb6be
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-05-05 09:48:04 +0300

    7 kyu HTML Generator

--- app.js
+++ app.js
@@ -1,3 +1,49 @@
-// https://www.codewars.com/kata/55b8c0276a7930249e00003c
-const encryption = (message) =>
-	[...message].map((char) => CHAR_TO_MORSE[char]).join(' ');
+// https://www.codewars.com/kata/54eecc187f9142cc4600119e
+
+class HTMLGen {
+	constructor() {}
+
+	generateTag(tag, content) {
+		return `<${tag}>${content}</${tag}>`;
+	}
+
+	div(content) {
+		return this.generateTag('div', content);
+	}
+	p(content) {
+		return this.generateTag('p', content);
+	}
+	b(content) {
+		return this.generateTag('b', content);
+	}
+	a(content) {
+		return this.generateTag('a', content);
+	}
+	body(content) {
+		return this.generateTag('body', content);
+	}
+	span(content) {
+		return this.generateTag('span', content);
+	}
+	title(content) {
+		return this.generateTag('title', content);
+	}
+	comment(content) {
+		return `<!--${content}-->`;
+	}
+}
+
+//   this.div=s=>"<div>"+s+"</div>";
+// this.b=s=>"<b>"+s+"</b>";
+// this.p=s=>"<p>"+s+"</p>";
+// this.a=s=>"<a>"+s+"</a>";
+// this.body=s=>"<body>"+s+"</body>";
+// this.span=s=>"<span>"+s+"</span>";
+// this.title=s=>"<title>"+s+"</title>";
+// this.comment=s=>"<!--"+s+"-->";
+
+let g = new HTMLGen();
+
+const par = g.p('Hello, world');
+const block = g.div(par);
+console.log(block);
