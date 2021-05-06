dd27f5e12676e6b7844af0ee9abc99f8e08fd56a
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-05-06 19:48:05 +0300

    1128. Number of Equivalent Domino Pairs

--- leetcode/1128. Number of Equivalent Domino Pairs.js
+++ leetcode/1128. Number of Equivalent Domino Pairs.js
@@ -1,4 +1,9 @@
 // 1128. Number of Equivalent Domino Pairs
+/*
+Given a list of dominoes, dominoes[i] = [a, b] is equivalent to dominoes[j] = [c, d] if and only if either (a==c and b==d), or (a==d and b==c) - that is, one domino can be rotated to be equal to another domino.
+
+Return the number of pairs (i, j) for which 0 <= i < j < dominoes.length, and dominoes[i] is equivalent to dominoes[j].
+*/
 
 const numEquivDominoPairs = (dominoes) =>
 	Object.values(
@@ -11,38 +16,3 @@ const numEquivDominoPairs = (dominoes) =>
 		.filter((fr) => fr > 1)
 		.map((n) => (n * (n - 1)) / 2)
 		.reduce((a, b) => a + b);
-
-console.log(
-	numEquivDominoPairs([
-		[2, 1],
-		[5, 4],
-		[3, 7],
-		[6, 2],
-		[4, 4],
-		[1, 8],
-		[9, 6],
-		[5, 3],
-		[7, 4],
-		[1, 9],
-		[1, 1],
-		[6, 6],
-		[9, 6],
-		[1, 3],
-		[9, 7],
-		[4, 7],
-		[5, 1],
-		[6, 5],
-		[1, 6],
-		[6, 1],
-		[1, 8],
-		[7, 2],
-		[2, 4],
-		[1, 6],
-		[3, 1],
-		[3, 9],
-		[3, 7],
-		[9, 1],
-		[1, 9],
-		[8, 9],
-	])
-);
