11446f52208813447d7be95efb0b41c413120a7d
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-05-06 14:57:45 +0300

    1078. Occurrences After Bigram

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-// https://www.codewars.com/kata/564e7fc20f0b53eb02000106
+// 1078. Occurrences After Bigram
 
-const consonantCount = (str) =>
-	[...str.replace(/[^A-Za-z]+/g, '')].filter(
-		(char) => !'aeiou'.includes(char.toLowerCase())
-	).length;
+/*
+Given words first and second, consider occurrences in some text of the form "first second third", where second comes immediately after first, and third comes immediately after second.
 
-console.log(consonantCount('sda1xzZz1))@'));
+For each such occurrence, add "third" to the answer, and return the answer.
+*/
+
+const findOcurrences = (text, first, second) => {
+	const words = text.split(' ');
+	let [i, j, k] = [0, 1, 2];
+	const res = [];
+
+	while (k < words.length) {
+		if (words[i] === first && words[j] === second) {
+			words[k] && res.push(words[k]);
+		}
+		i++;
+		j++;
+		k++;
+	}
+	return res;
+};
+
+console.log(
+	findOcurrences('alice is a good girl she is a good student', 'a', 'good')
+);
