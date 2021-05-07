ed6de2029ee735b6a78b79e9e1446fa748775877
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-05-07 10:56:13 +0300

    7 kyu String to integer conversion

--- 7 kyu String to integer conversion.js
+++ 7 kyu String to integer conversion.js
@@ -1,7 +1,3 @@
 // https://www.codewars.com/kata/54fdadc8762e2e51e400032c
 
 const myParseInt = (str) => (str.trim().match(/[\D]/) ? NaN : parseInt(str));
-
-console.log(myParseInt('1 2 3'));
-
-console.log(parseInt('1 2 3'));
