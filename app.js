68162ed5642f73267aec4305ace3a8a922da6943
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-05-09 15:18:59 +0300

    7 kyu Radio DJ helper function

--- app.js
+++ app.js
@@ -1,5 +1,44 @@
-// https://www.codewars.com/kata/563c39f92f7bed9572000011
+// https://www.codewars.com/kata/561bbcb0fbbfb0f5010000ee
 
-const isNegativeZero = (x) => Object.is(x, -0);
+const songs = [
+	{ artist: 'Marillion', title: 'Keyleigh', playback: '03:36' },
+	{ artist: 'Pink Floyd', title: 'Time', playback: '06:48' },
+	{ artist: 'Rush', title: 'YYZ', playback: '04:27' },
+	{ artist: 'Bonobo', title: 'Days To Come', playback: '03:50' },
+	{ artist: 'Coldplay', title: 'Yellow', playback: '04:32' },
+	{ artist: 'Bloc Party', title: 'Like Eating Glass', playback: '04:22' },
+	{ artist: 'The Killers', title: 'For Reasons Unknown', playback: '03:30' },
+	{ artist: 'Arctic Monkeys', title: 'Teddy Picker', playback: '03:25' },
+	{
+		artist: 'Joe Satriani',
+		title: 'Surfing With The Alien',
+		playback: '04:34',
+	},
+];
 
-console.log(isNegativeZero(0));
+const convertToSeconds = (time) =>
+	time
+		.split(':')
+		.reduce((sec, t, id) => (id === 0 ? sec + parseInt(t) * 60 : sec + +t), 0);
+
+const longestPossible = (playback) => {
+	let currentlyLongest;
+	let proximity = +Infinity;
+
+	for (let song of songs) {
+		let durationDifference = playback - convertToSeconds(song.playback);
+
+		if (!durationDifference) return song.title;
+
+		if (durationDifference > 0) {
+			if (durationDifference < proximity) {
+				currentlyLongest = song.title;
+			}
+			proximity = Math.min(durationDifference, proximity);
+		}
+	}
+
+	return currentlyLongest ? currentlyLongest : false;
+};
+
+console.log(longestPossible(215));
