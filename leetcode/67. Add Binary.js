6a4d36ed6baa8241e1663916d144bcec8d276f76
Vova Ushenko <vovaushenko1989@gmail.com>
2021-06-10 09:54:42 +0300

    67. Add Binary

--- leetcode/67. Add Binary.js
+++ leetcode/67. Add Binary.js
@@ -5,5 +5,3 @@ Given two binary strings a and b, return their sum as a binary string.
 */
 
 const addBinary = (a, b) => (BigInt(`0b${a}`) + BigInt(`0b${b}`)).toString(2);
-
-console.log(addBinary('1010', '1011'));
