e01a52674cc4a8b8862cbb670aa3b71827e8238f
Vova Ushenko <vovaushenko1989@gmail.com>
2021-06-14 15:50:52 +0300

    7 kyu Heron's formula

--- 7 kyu Heron's formula.js
+++ 7 kyu Heron's formula.js
@@ -4,5 +4,3 @@ const calcS = (a, b, c) => (a + b + c) / 2;
 
 const heron = (a, b, c, s = calcS(a, b, c)) =>
 	parseInt(Math.sqrt(s * (s - a) * (s - b) * (s - c)).toFixed(2));
-
-console.log(heron(3, 4, 5));
