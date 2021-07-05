d00fe1796fb4efcf3410df9033c75ae6f6207ccc
vovaushenko <vovaushenko1989@gmail.com>
2021-07-05 16:29:22 +0300

    524. Longest Word in Dictionary through Deleting

--- leetcode/524. Longest Word in Dictionary through Deleting.js
+++ leetcode/524. Longest Word in Dictionary through Deleting.js
@@ -1,7 +1,9 @@
 // https://leetcode.com/problems/longest-word-in-dictionary-through-deleting/
 
+/*
+Given a string s and a string array dictionary, return the longest string in the dictionary that can be formed by deleting some of the given string characters. If there is more than one possible result, return the longest word with the smallest lexicographical order. If there is no possible result, return the empty string.
+*/
 const findLongestWord = (s, dictionary) => {
-	// sort dict
 	dictionary.sort((w1, w2) =>
 		w1.length === w2.length ? w1.localeCompare(w2) : w2.length - w1.length
 	);
@@ -17,5 +19,3 @@ const findLongestWord = (s, dictionary) => {
 
 	return '';
 };
-
-console.log(findLongestWord('abpcplea', ['ale', 'apple', 'monkey', 'plea']));
