85f99a9f0a9aff4134b480bf52ed2900234d31c8
vovaushenko <vovaushenko1989@gmail.com>
2021-07-11 13:58:27 +0300

    1929. Concatenation of Array

--- leetcode/1929. Concatenation of Array.js
+++ leetcode/1929. Concatenation of Array.js
@@ -1,4 +1,7 @@
 // https://leetcode.com/problems/concatenation-of-array/
+
+const getConcatenation = (nums) => [...nums, ...nums];
+// https://leetcode.com/problems/concatenation-of-array/
 /*
 Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
 
