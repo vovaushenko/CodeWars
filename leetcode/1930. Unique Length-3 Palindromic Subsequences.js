2e8515a0ca63d859f32cb1b6fafac9b52921efa3
vovaushenko <vovaushenko1989@gmail.com>
2021-07-12 13:54:57 +0300

    1930. Unique Length-3 Palindromic Subsequences

--- leetcode/1930. Unique Length-3 Palindromic Subsequences.js
+++ leetcode/1930. Unique Length-3 Palindromic Subsequences.js
@@ -1,3 +1,15 @@
+//! 1930. Unique Length-3 Palindromic Subsequences
+/*
+Given a string s, return the number of unique palindromes of length three that are a subsequence of s.
+
+Note that even if there are multiple ways to obtain the same subsequence, it is still only counted once.
+
+A palindrome is a string that reads the same forwards and backwards.
+
+A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+
+For example, "ace" is a subsequence of "abcde".*/
+
 const countPalindromicSubsequence = (s) => {
 	const palindromes = {};
 	let seen = {};
@@ -32,5 +44,3 @@ const countPalindromicSubsequence = (s) => {
 	}
 	return palindromeCount;
 };
-
-countPalindromicSubsequence('aabca');
