e6559f33ddad1c7067d5b9755c2c1264295331ed
vovaushenko <vovaushenko1989@gmail.com>
2021-07-28 15:34:55 +0300

    74. Search a 2D Matrix

--- leetcode/74. Search a 2D Matrix.js
+++ leetcode/74. Search a 2D Matrix.js
@@ -25,14 +25,3 @@ const searchMatrix = (matrix, target) => {
 
 	return flat[mid] === target;
 };
-
-console.log(
-	searchMatrix(
-		[
-			[1, 3, 5, 7],
-			[10, 11, 16, 20],
-			[23, 30, 34, 60],
-		],
-		30
-	)
-);
