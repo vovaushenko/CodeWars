bcc33d3972e22fd607a98eb826d4598cf5f5c10f
vovaushenko <vovaushenko1989@gmail.com>
2021-08-05 19:43:22 +0300

    Leetcoding

---
+++ leetcode/877. Stone Game.js
@@ -0,0 +1,25 @@
+// 877. Stone Game
+
+/*
+Alex and Lee play a game with piles of stones.  There are an even number of piles arranged in a row, and each pile has a positive integer number of stones piles[i].
+
+The objective of the game is to end with the most stones.  The total number of stones is odd, so there are no ties.
+
+Alex and Lee take turns, with Alex starting first.  Each turn, a player takes the entire pile of stones from either the beginning or the end of the row.  This continues until there are no more piles left, at which point the person with the most stones wins.
+
+Assuming Alex and Lee play optimally, return True if and only if Alex wins the game.
+*/
+
+const stoneGame = (piles) => {
+	piles = piles.sort((a, b) => b - a);
+	let alex = 0;
+	let lee = 0;
+	let counter = 0;
+
+	for (let pile of piles) {
+		counter % 2 === 0 ? (alex += pile) : (lee += pile);
+		counter++;
+	}
+
+	return alex > lee;
+};
