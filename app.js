922e05ec8ac77c5e5d311fe478dbabaa207eb14a
vovaushenko <vovaushenko1989@gmail.com>
2021-11-20 09:48:16 +0200

    test

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-const isValidWord = (token) => {
-	return /^(([a-z]?)|([a-z]+\-?[a-z]+))[!.,]?$/.test(token);
+const findSecondMinimumValue = (root, smallest = [Infinity, Infinity]) => {
+	if (!root) return;
+
+	if (!smallest.includes(root.val) && root.val < smallest[1]) {
+		smallest.pop();
+		smallest.push(root.val);
+		smallest.sort((a, b) => a - b);
+	}
+
+	root.left && findSecondMinimumValue(root.left, smallest);
+	root.right && findSecondMinimumValue(root.right, smallest);
+
+	console.log(smallest);
+
+	return smallest[1];
 };
 
-const countValidWords = (sentence) =>
-	sentence
-		.split(' ')
-		.filter((_) => _)
-		.filter((token) => isValidWord(token)).length;
-
-console.log(isValidWord(''));
-
-console.log(isValidWord('dsa'));
-
-console.log(countValidWords('cat and  dog'));
+console.log(findSecondMinimumValue({ val: 5 }));
--- problem.ts
+++ problem.ts
@@ -1,98 +1,15 @@
-interface ITreeNode {
-	val: number;
-	left: null | ITreeNode;
-	right: null | ITreeNode;
-}
+const pluck = <DataType, KeyType extends keyof DataType>(
+	items: DataType[],
+	key: KeyType
+): DataType[KeyType][] => {
+	return items.map((item) => item[key]);
+};
 
-export class TreeNode implements ITreeNode {
-	val: number;
-	left: null | ITreeNode;
-	right: null | ITreeNode;
-	constructor(val: number) {
-		this.val = val;
-		this.left = null;
-		this.right = null;
-	}
-}
+const dogs = [
+	{ name: 'Mimi', age: 12 },
+	{ name: 'LG', age: 13 },
+	{ name: 'BigBoy', age: 3 },
+	{ name: 'Lisa', age: 5 },
+];
 
-export class BST {
-	root: null | ITreeNode;
-
-	constructor() {
-		this.root = null;
-	}
-
-	push(val: number): void {
-		const newNode = new TreeNode(val);
-		if (this.root === null) {
-			this.root = newNode;
-			return;
-		} else {
-			let current = this.root;
-			while (true) {
-				if (val < current.val) {
-					if (!current.left) {
-						current.left = newNode;
-						return;
-					} else {
-						current = current.left;
-					}
-				} else if (val > current.val) {
-					if (!current.right) {
-						current.right = newNode;
-						return;
-					} else {
-						current = current.right;
-					}
-				} else {
-					throw new Error('This node already exists');
-				}
-			}
-		}
-	}
-
-	dfs(): number[] {
-		if (!this.root) return [];
-		const values: Array<number> = [];
-
-		const traverse = (node: TreeNode): void => {
-			if (!node) return;
-			values.push(node.val);
-			if (node.left) traverse(node.left);
-			if (node.right) traverse(node.right);
-		};
-
-		traverse(this.root);
-		return values;
-	}
-
-	bfs(): number[] {
-		if (!this.root) return [];
-		const values: Array<number> = [];
-		const queue: Array<TreeNode> = [this.root];
-
-		while (queue.length) {
-			let node = queue.shift();
-			if (node) {
-				values.push(node.val);
-				if (node.left) queue.push(node.left);
-				if (node.right) queue.push(node.right);
-			}
-		}
-
-		return values;
-	}
-}
-
-const tree = new BST();
-tree.push(12);
-tree.push(10);
-tree.push(11);
-tree.push(6);
-tree.push(15);
-tree.push(13);
-tree.push(77);
-console.log(tree);
-
-console.log(tree.dfs());
-console.log(tree.bfs());
+console.log(pluck(dogs, 'name'));
