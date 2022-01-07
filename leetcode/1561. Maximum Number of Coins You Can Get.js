cef324906b10cdcebb4adb9971bf1a87d5d67f64
vovaushenko <vovaushenko1989@gmail.com>
2022-01-07 15:48:22 -0800

    Leetcoding

---
+++ leetcode/1561. Maximum Number of Coins You Can Get.js
@@ -0,0 +1,19 @@
+// 1561. Maximum Number of Coins You Can Get
+/*
+There are 3n piles of coins of varying size, you and your friends will take piles of coins as follows:
+
+In each step, you will choose any 3 piles of coins (not necessarily consecutive).
+Of your choice, Alice will pick the pile with the maximum number of coins.
+You will pick the next pile with the maximum number of coins.
+Your friend Bob will pick the last pile.
+Repeat until there are no more piles of coins.
+Given an array of integers piles where piles[i] is the number of coins in the ith pile.
+
+Return the maximum number of coins that you can have.
+*/
+const maxCoins = (piles) => {
+	piles.sort((a, b) => a - b);
+	let res = 0;
+	for (let i = piles.length / 3; i < piles.length; i += 2) res += piles[i];
+	return res;
+};
