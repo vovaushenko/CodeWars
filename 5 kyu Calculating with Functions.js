2d8517f38d7518727e05c974d37f46823fad75a5
vovaushenko <vovaushenko1989@gmail.com>
2022-01-09 18:46:55 -0800

    Codewars

---
+++ 5 kyu Calculating with Functions.js
@@ -0,0 +1,18 @@
+/*
+5 kyu
+Calculating with Functions
+*/
+const plus = (x) => (y) => x + y;
+const minus = (x) => (y) => y - x;
+const dividedBy = (x) => (y) => ~~(y / x);
+const times = (x) => (y) => x * y;
+const zero = (f) => (f ? f(0) : 0);
+const one = (f) => (f ? f(1) : 1);
+const two = (f) => (f ? f(2) : 2);
+const three = (f) => (f ? f(3) : 3);
+const four = (f) => (f ? f(4) : 4);
+const five = (f) => (f ? f(5) : 5);
+const six = (f) => (f ? f(6) : 6);
+const seven = (f) => (f ? f(7) : 7);
+const eight = (f) => (f ? f(8) : 8);
+const nine = (f) => (f ? f(9) : 9);
