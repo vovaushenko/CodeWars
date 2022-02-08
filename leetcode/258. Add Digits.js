c117e698c871cbc56bc485f6c9758941f9cedfba
vovaushenko <vovaushenko1989@gmail.com>
2022-02-07 20:07:48 -0800

    leetcoding

--- leetcode/258. Add Digits.js
+++ leetcode/258. Add Digits.js
@@ -3,9 +3,16 @@
 const addDigitsOfNumber = (n) => [...(n + '')].reduce((a, b) => a + +b, 0) + '';
 
 const addDigits = (num) => {
-    num = addDigitsOfNumber(num);
+	num = addDigitsOfNumber(num);
 
-    while (num.length > 1) num = addDigitsOfNumber(num);
+	while (num.length > 1) num = addDigitsOfNumber(num);
 
-    return +num;
+	return +num;
 };
+
+// recursion
+const length = (number) => String(number).length;
+const digitsSum = (number) => [...String(number)].reduce((a, b) => a + +b, 0);
+
+const addDigits = (num) =>
+	length(num) === 1 ? num : addDigits(digitsSum(num));
