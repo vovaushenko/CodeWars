b40608e4b9f88eac1c837475e75e4cbddd6bcc35
vovaushenko <vovaushenko1989@gmail.com>
2022-02-13 18:11:37 -0800

    Leetcoding

---
+++ leetcode/1963. Minimum Number of Swaps to Make the String Balanced.js
@@ -0,0 +1,25 @@
+//1963. Minimum Number of Swaps to Make the String Balanced
+/*
+You are given a 0-indexed string s of even length n. The string consists of exactly n / 2 opening brackets '[' and n / 2 closing brackets ']'.
+
+A string is called balanced if and only if:
+
+It is the empty string, or
+It can be written as AB, where both A and B are balanced strings, or
+It can be written as [C], where C is a balanced string.
+You may swap the brackets at any two indices any number of times.
+
+Return the minimum number of swaps to make s balanced.
+*/
+const minSwaps = (s) => {
+	let count = 0;
+	for (const par of s) {
+		if (par === ']') {
+			count++;
+		} else {
+			if (count) count--;
+		}
+	}
+
+	return Math.ceil(count / 2);
+};
