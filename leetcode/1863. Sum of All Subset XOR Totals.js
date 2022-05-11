47d1b04c08526633080ec1881b5116d117427d18
vovaushenko <vovaushenko1989@gmail.com>
2022-05-10 20:35:18 -0700

    leetcoding

---
+++ leetcode/1863. Sum of All Subset XOR Totals.js
@@ -0,0 +1,21 @@
+//1863. Sum of All Subset XOR Totals
+
+const subsetXORSum = (nums) => {
+	let output = [];
+
+	const backtrack = (start = 0, arr = [nums[0]]) => {
+		output.push([...arr].reduce((a, b) => a ^ b, 0));
+
+		for (let i = start; i < nums.length; i++) {
+			arr.push(nums[i]);
+
+			backtrack(i + 1, arr);
+
+			arr.pop();
+		}
+	};
+
+	backtrack();
+
+	return output.reduce((a, b) => a + b);
+};
