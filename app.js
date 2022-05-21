2c2114ff30ace058ddf49a4cb3c5b596c3abc069
vovaushenko <vovaushenko1989@gmail.com>
2022-05-21 08:42:52 -0700

    leetcoding

--- app.js
+++ app.js
@@ -1,19 +1,3 @@
-'use strict';
+//2273. Find Resultant Array After Removing Anagrams
 
-const convert = function (s, numRows) {
-	const result = [];
-	let [step, index] = [1, 0];
-	for (let i = 0; i < s.length; i++) {
-		if (result[index] === undefined) {
-			result[index] = '';
-		}
-		result[index] += s[i];
-		if (index === 0) {
-			step = 1;
-		} else if (index === numRows - 1) {
-			step = -1;
-		}
-		index += step;
-	}
-	return result.join('');
-};
+// 2274. Maximum Consecutive Floors Without Special Floors
