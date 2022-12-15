fff4acde1757445bd917b0471139fc9d9f5c53c1
vovaushenko <vovaushenko1989@gmail.com>
2022-12-14 20:06:51 -0800

    leetcoding

---
+++ leetcode/2501. Longest Square Streak in an Array.js
@@ -0,0 +1,25 @@
+//2501. Longest Square Streak in an Array
+/*
+You are given an integer array nums. A subsequence of nums is called a square streak if:
+
+The length of the subsequence is at least 2, and
+after sorting the subsequence, each element (except the first element) is the square of the previous number.
+Return the length of the longest square streak in nums, or return -1 if there is no square streak.
+
+A subsequence is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements.
+*/
+const longestSquareStreak = (nums) => {
+	const map = nums.reduce((h, n) => ((h[n] = true), h), {});
+	let maxStreak = 0;
+
+	for (const num of nums) {
+		let start = num;
+		let currentStreak = 0;
+		while (start in map) {
+			start *= start;
+			currentStreak++;
+		}
+		maxStreak = Math.max(maxStreak, currentStreak);
+	}
+	return maxStreak === 1 ? -1 : maxStreak;
+};
