07dbb50c796d50fd8093bea3d45f5d7b4688b331
vovaushenko <vovaushenko1989@gmail.com>
2023-01-19 18:09:30 -0800

    leetcoding

--- leetcode/414. Third Maximum Number.js
+++ leetcode/414. Third Maximum Number.js
@@ -2,7 +2,23 @@
 // Given integer array nums, return the third maximum number in this array. If the third maximum does not exist, return the maximum number.
 
 const thirdMax = (nums) => {
-  nums = [...new Set(nums)].sort((a, b) => b - a);
+	nums = [...new Set(nums)].sort((a, b) => b - a);
 
-  return nums[2] || nums[2] === 0 ? nums[2] : nums[0];
+	return nums[2] || nums[2] === 0 ? nums[2] : nums[0];
+};
+
+const thirdMax = (nums) => {
+	let [first, second, third] = [-Infinity, -Infinity, -Infinity];
+	for (const num of nums) {
+		if (num === first || num === second || num === third) continue;
+		if (num > first) {
+			[first, second, third] = [num, first, second];
+		} else if (num > second) {
+			[second, third] = [num, second];
+		} else if (num > third) {
+			third = num;
+		}
+	}
+
+	return third === -Infinity ? first : third;
 };
