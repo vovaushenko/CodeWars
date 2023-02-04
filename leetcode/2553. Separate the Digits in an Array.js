27d27a2d96c32881496da59fe3bcf8a458aac0d0
vovaushenko <vovaushenko1989@gmail.com>
2023-02-04 14:48:28 -0800

    leetcoding

---
+++ leetcode/2553. Separate the Digits in an Array.js
@@ -0,0 +1,9 @@
+/*
+Given an array of positive integers nums, return an array answer that consists of the digits of each integer in nums after separating them in the same order they appear in nums.
+
+To separate the digits of an integer is to get all the digits it has in the same order.
+
+For example, for the integer 10921, the separation of its digits is [1,0,9,2,1].
+*/
+const separateDigits = (nums) =>
+	nums.reduce((res, num) => [...res, ...[...String(num)].map(Number)], []);
