6e9262316c61b15e33b080f03ba3b755912fe1dc
vovaushenko <vovaushenko1989@gmail.com>
2023-02-13 18:37:38 -0800

    leetcoding

---
+++ leetcode/2562. Find the Array Concatenation Value.js
@@ -0,0 +1,28 @@
+/*
+You are given a 0-indexed integer array nums.
+
+The concatenation of two numbers is the number formed by concatenating their numerals.
+
+For example, the concatenation of 15, 49 is 1549.
+The concatenation value of nums is initially equal to 0. Perform this operation until nums becomes empty:
+
+If there exists more than one number in nums, pick the first element and last element in nums respectively and add the value of their concatenation to the concatenation value of nums, then delete the first and last element from nums.
+If one element exists, add its value to the concatenation value of nums, then delete it.
+Return the concatenation value of the nums.
+*/
+const findTheArrayConcVal = (nums) => {
+	if (nums.length < 2) return nums[0];
+	let [i, j] = [0, nums.length - 1];
+	let total = 0;
+	while (i <= j) {
+		if (i !== j) {
+			total += Number(`${nums[i]}${nums[j]}`);
+		} else {
+			total += nums[i];
+		}
+		i++;
+		j--;
+	}
+
+	return total;
+};
