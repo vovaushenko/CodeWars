8a09fc92435c990537de2d05a9dcc3a1437e496a
vovaushenko <vovaushenko1989@gmail.com>
2023-06-14 14:57:50 -0700

    leetcoding

---
+++ leetcode/2715. Execute Cancellable Function With Delay.js
@@ -0,0 +1,29 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function (fn, args, t) {
+	const timerId = setTimeout(fn, t, ...args);
+	const cancel = () => clearTimeout(timerId);
+	return cancel;
+};
+
+/**
+ *  const result = []
+ *
+ *  const fn = (x) => x * 5
+ *  const args = [2], t = 20, cancelT = 50
+ *
+ *  const log = (...argsArr) => {
+ *      result.push(fn(...argsArr))
+ *  }
+ *
+ *  const cancel = cancellable(fn, args, t);
+ *
+ *  setTimeout(() => {
+ *     cancel()
+ *     console.log(result) // [{"time":20,"returned":10}]
+ *  }, cancelT)
+ */
