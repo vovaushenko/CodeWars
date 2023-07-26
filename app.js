fc1888e5d19172196da812cc088b288089c2007c
vovaushenko <vovaushenko1989@gmail.com>
2023-07-25 21:30:32 -0700

    leetcoding

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-//2273. Find Resultant Array After Removing Anagrams
+var countNumbersWithUniqueDigits = function (n) {
+	if (n === 0) return 1;
+	if (n === 1) return 10;
+	if (n > 10) return countNumbersWithUniqueDigits(10);
 
-// 2274. Maximum Consecutive Floors Without Special Floors
+	return countNumbersWithUniqueDigits(n - 1) + 9 * cmb(10 - n);
+};
+
+function cmb(n) {
+	var result = 9;
+	while (n < 8) {
+		result *= n + 1;
+		n++;
+	}
+	return result;
+}
